2670bb0125ee05d18f39cbaf58f8ef9999aab028
Paul Lancaster <pl59@st-andrews.ac.uk>
2016-10-07 09:41:32 +0100

    Updated information on some structures

--- src/Structure.java
+++ src/Structure.java
@@ -5,11 +5,11 @@ public enum Structure {
     // Data on VSPER structures
     // Angle one, angle 2, ligand count, lone pair count
     LINEAR(180,0,2,0),
-    BENT_ONE(),
+    BENT_ONE(120,),
     BENT_TWO(),
-    TRIGONAL_PLANAR(120, ),
-    TETRAHEDRAL(),
-    OCTAHEDRAL(),
+    TRIGONAL_PLANAR(120,0,3,0),
+    TETRAHEDRAL(109,0,4,0),
+    OCTAHEDRAL(90,0,6,0),
 
     ;
 
